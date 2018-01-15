eb7eee90591ac5ac28a630f1a25a289772157016
yossigijrath <yossi.gijrath@gmail.com>
2018-01-15 12:24:39 +0100

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,24 @@
+
+
+#class for planes
+class Plane(object):
+    def __init__(self):
+        self.plane_list = []
+
+#class for destinations
+class Location(object):
+    def __init__(self):
+        self.location_list = []
+
+#class for passengers
+class Passenger(object):
+    def __init__(self):
+        self.passenger_list = []
+
+#constraint boarding time
+
+#constraint fuel
+
+#constraint detour
+
+#constraint flight time
