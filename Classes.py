24caf433ffd98ae55e56c357b4937888fa69d059
yossigijrath <yossi.gijrath@gmail.com>
2018-01-15 14:52:16 +0100

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -2,8 +2,28 @@
 
 #class for planes
 class Plane(object):
+
+FUEL_START = 3199
+SPEED_PLANE = 800
+SEATS_IN_PLANE = 199
+BOARDING_TIME = 1
+UNBOARDING_TIME = 1
+REFUEL_TIME = 1
+
     def __init__(self):
         self.plane_list = []
+
+    def speed(self):
+
+    def fuel(self):
+
+    def seats(self):
+
+    def refueling(self):
+
+    def boarding(self):
+
+    def un_boarding(self):
 
 #class for destinations
 class Location(object):
