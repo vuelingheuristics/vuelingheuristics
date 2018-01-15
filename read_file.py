8410262d6c2a590511eb7cc861ecc6ceccd238c1
yossigijrath <yossi.gijrath@gmail.com>
2018-01-15 16:25:01 +0100

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -7,6 +7,7 @@ cor_x = []
 cor_y = []
 loc_z = []
 
+
 def reading_data_locations():
     with open('MokumAirwaysCities.txt','r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
@@ -15,21 +16,21 @@ def reading_data_locations():
             cor_x.append(int(row[1]))
             cor_y.append(int(row[2]))
             loc_z.append(str(row[3]))
-        return (loc_a, cor_x, cor_y, loc_z)
+        print (loc_a, cor_x, cor_y, loc_z)
 
 def reading_data_distances():
     with open('MokumAirwaysDistances.txt', 'r') as list_distances:
         for lines in list_distances:
             line = lines.splitlines()
             matrix = np.array(line)
-        return (matrix)
+            print(matrix)
 
 def reading_data_transport():
     with open('MokumAirwaysPassengers.txt', 'r') as list_passengers:
         for lines in list_passengers:
             line = lines.splitlines()
             matrix = np.array(line)
-        return (matrix)
+            print(matrix)
 
 def plot_data():
     reading_data_locations()
